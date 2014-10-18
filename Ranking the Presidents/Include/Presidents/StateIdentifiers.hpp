f49b944c3629658dced0bb4f00b66d12c9caf084
aaronistheman2 <aarons.7007@gmail.com>
2014-10-18 14:29:26 -0700

    Added classes State and StateStack, identifiers for resources and states, and Foreach.hpp

---
+++ Ranking the Presidents/Include/Presidents/StateIdentifiers.hpp
@@ -0,0 +1,20 @@
+#ifndef PRESIDENTS_STATEIDENTIFIERS_HPP
+#define PRESIDENTS_STATEIDENTIFIERS_HPP
+
+
+namespace States
+{
+	enum ID
+	{
+		None,
+		Title,
+		Menu,
+		Game,
+		Loading,
+		Pause,
+    Settings,
+    GameOver
+	};
+}
+
+#endif // PRESIDENTS_STATEIDENTIFIERS_HPP
