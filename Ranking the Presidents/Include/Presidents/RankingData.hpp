6eea965c94043cff54e6f381146a950764452725
aaronistheman <aarons.7007@gmail.com>
2014-12-22 23:20:29 -0800

    Added a class that represents data for a president in the RankingsState

---
+++ Ranking the Presidents/Include/Presidents/RankingData.hpp
@@ -0,0 +1,19 @@
+#ifndef PRESIDENTS_RANKINGDATA_HPP
+#define PRESIDENTS_RANKINGDATA_HPP
+
+#include <string>
+
+
+// Contains the rankings data for a president.
+// For use in RankingsState.
+struct RankingData
+{
+  int number;
+  std::string name;
+  int rank;
+  int peaceRating;
+  int prosperityRating;
+  int libertyRating;
+};
+
+#endif // PRESIDENTS_RANKINGDATA_HPP
