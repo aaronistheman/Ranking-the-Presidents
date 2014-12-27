f07a76f9cfee95bfba51f48f43cb432ec47443c3
aaronistheman <aarons.7007@gmail.com>
2014-12-27 10:34:53 -0800

    Removed any reference to RankingData, as well as its respective file

--- Ranking the Presidents/Include/Presidents/DataTables.hpp
+++ Ranking the Presidents/Include/Presidents/DataTables.hpp
@@ -2,12 +2,11 @@
 #define PRESIDENTS_DATATABLES_HPP
 
 #include <Presidents/DescriptionData.hpp>
-#include <Presidents/RankingData.hpp>
 
 #include <vector>
 
-// Each of these functions closes the file it reads from
+
+// Closes the file it reads from
 std::vector<DescriptionData> initializeDescriptionData();
-std::vector<RankingData> initializeRankingData();
 
 #endif // PRESIDENTS_DATATABLES_HPP
